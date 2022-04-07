bd82c4419f6cbb66a59fa6c0359a8bfb880f4978
nisha4545 <88261286+nisha4545@users.noreply.github.com>
2022-04-07 21:43:56 +0530

    badge icon added

--- src/components/Header/ActionsHead/ActionsHead.js
+++ src/components/Header/ActionsHead/ActionsHead.js
@@ -3,21 +3,26 @@ import { Link } from "react-router-dom";
 import PermIdentityOutlinedIcon from '@mui/icons-material/PermIdentityOutlined';
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
+import { useAllData } from '../../../context/AllDataContext';
+import "./ActionsHead.css"
 
 function ActionsHead() {
+  const {state:{wishlist,bag}}=useAllData();
   return (
     <div className="actions-head flex-vCenter">
     <Link to="/login" className="user flex-col flex-vCenter">
         <span><PermIdentityOutlinedIcon/> </span>
         <span className="actions-title">Profile</span>
     </Link>
-    <Link to="/wishlist" className="wishlist flex-col flex-vCenter">
+    <Link to="/wishlist" className="wishlist wish-badge flex-col flex-vCenter">
         <span><FavoriteBorderOutlinedIcon size={24}px/></span>
         <span className="actions-title">Wishlist</span>
+        {wishlist.length>1?<span className="status-badge flex-center">{wishlist.length}</span>:null}
     </Link>
-    <Link to="/bag" className="bag flex-col flex-vCenter">
+    <Link to="/bag" className="bag bag-badge flex-col flex-vCenter">
         <span><ShoppingBagIcon/> </span>
         <span className="actions-title">Bag</span>
+        {bag.length>1?<span className="status-badge bag-status flex-center">{bag.length}</span>:null}
     </Link>
 </div>
   )
